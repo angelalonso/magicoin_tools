c3d09feba5d11fcfa6a552c38c077398f9860fcd
Angel Alonso <alonsofonseca.angel@gmail.com>
2017-10-31 16:13:47 +0100

    working script

--- get_values.sh
+++ get_values.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+# Script to get current value of Magicoin and Bitcoin
+# Currently being run as a daily cronjob
+
 WORKDIR="/home/sysadm/magicoin_tools"
 DATE=$(LC_ALL=en_US.utf8 date +"%b %d, %Y")
 
 # Magicoin
 URL="https://api.coinmarketcap.com/v1/ticker/magi/"
-VALS=$(curl ${URL} | jq -r '[.[].price_usd,.[]["24h_volume_usd"],.[].market_cap_usd]' | jq -r ' @csv' | sed 's/"//g' | sed 's/,/;/g')
+VALS=$(curl -s ${URL} | jq -r '[.[].price_usd,.[]["24h_volume_usd"],.[].market_cap_usd]' | jq -r ' @csv' | sed 's/"//g' | sed 's/,/;/g')
 #echo "Date;Open;High;Low;Close;Volume;Market Cap"
 echo "${DATE};;;;${VALS}" >> ${WORKDIR}/magicoin.hist
 
 # Bitcoint
 URL="https://api.coinmarketcap.com/v1/ticker/bitcoin/"
-VALS=$(curl ${URL} | jq -r '[.[].price_usd,.[]["24h_volume_usd"],.[].market_cap_usd]' | jq -r ' @csv' | sed 's/"//g' | sed 's/,/;/g')
+VALS=$(curl -s ${URL} | jq -r '[.[].price_usd,.[]["24h_volume_usd"],.[].market_cap_usd]' | jq -r ' @csv' | sed 's/"//g' | sed 's/,/;/g')
 #echo "Date;Open;High;Low;Close;Volume;Market Cap"
 echo "${DATE};;;;${VALS}" >> ${WORKDIR}/bitcoin.hist
 
