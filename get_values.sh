ee7a670cde96266401c136fa5a7efc7bc287726a
Angel Alonso <alonsofonseca.angel@gmail.com>
2017-10-31 16:05:39 +0100

    moving data to files

--- get_values.sh
+++ get_values.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
+WORKDIR="/home/sysadm/magicoin_tools"
 DATE=$(LC_ALL=en_US.utf8 date +"%b %d, %Y")
 
+# Magicoin
 URL="https://api.coinmarketcap.com/v1/ticker/magi/"
-
 VALS=$(curl ${URL} | jq -r '[.[].price_usd,.[]["24h_volume_usd"],.[].market_cap_usd]' | jq -r ' @csv' | sed 's/"//g' | sed 's/,/;/g')
 #echo "Date;Open;High;Low;Close;Volume;Market Cap"
-echo "${DATE};;;;${VALS}" 
+echo "${DATE};;;;${VALS}" >> ${WORKDIR}/magicoin.hist
 
-#URL="https://api.coinmarketcap.com/v1/ticker/bitcoin/"
-#
-#curl ${URL}
+# Bitcoint
+URL="https://api.coinmarketcap.com/v1/ticker/bitcoin/"
+VALS=$(curl ${URL} | jq -r '[.[].price_usd,.[]["24h_volume_usd"],.[].market_cap_usd]' | jq -r ' @csv' | sed 's/"//g' | sed 's/,/;/g')
+#echo "Date;Open;High;Low;Close;Volume;Market Cap"
+echo "${DATE};;;;${VALS}" >> ${WORKDIR}/bitcoin.hist
 
