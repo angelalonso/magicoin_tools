d0bbe59bbb4faad6c08b57be86feedc53c424d37
Angel Alonso <alonsofonseca.angel@gmail.com>
2017-11-02 17:00:55 +0100

    values by 1 nov 2017

--- get_values.sh
+++ get_values.sh
@@ -3,17 +3,18 @@
 # Currently being run as a daily cronjob
 
 WORKDIR="/home/sysadm/magicoin_tools"
-DATE=$(LC_ALL=en_US.utf8 date +"%b %d, %Y")
+#Old one: DATE=$(LC_ALL=en_US.utf8 date +"%b %d, %Y")
+DATE=$(LC_ALL=en_US.utf8 date +"%Y%m%d")
 
 # Magicoin
 URL="https://api.coinmarketcap.com/v1/ticker/magi/"
 VALS=$(curl -s ${URL} | jq -r '[.[].price_usd,.[]["24h_volume_usd"],.[].market_cap_usd]' | jq -r ' @csv' | sed 's/"//g' | sed 's/,/;/g')
 #echo "Date;Open;High;Low;Close;Volume;Market Cap"
-echo "${DATE};;;;${VALS}" >> ${WORKDIR}/magicoin.hist
+echo "${DATE};;;;${VALS}" >> ${WORKDIR}/magicoin.csv
 
 # Bitcoint
 URL="https://api.coinmarketcap.com/v1/ticker/bitcoin/"
 VALS=$(curl -s ${URL} | jq -r '[.[].price_usd,.[]["24h_volume_usd"],.[].market_cap_usd]' | jq -r ' @csv' | sed 's/"//g' | sed 's/,/;/g')
 #echo "Date;Open;High;Low;Close;Volume;Market Cap"
-echo "${DATE};;;;${VALS}" >> ${WORKDIR}/bitcoin.hist
+echo "${DATE};;;;${VALS}" >> ${WORKDIR}/bitcoin.csv
 
